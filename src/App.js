f2db5105f8f5681fd1dbf796fd6cea80c8e6dbec
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-07-03 15:54:09 +0000

    Feat: Basic react router navigation setup.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 import NavBar from "./components/NavBar";
+import Homepage from "./components/Homepage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
     </div>
   );
 }
