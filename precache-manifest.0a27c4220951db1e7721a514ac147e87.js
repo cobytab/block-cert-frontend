3e20d4532d11ca55104815ff028fa8f88a994fe3
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-07-06 19:42:37 +0000

    Updates

---
+++ precache-manifest.0a27c4220951db1e7721a514ac147e87.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe07ffeb3182cef91f0aff6879ebe49",
+    "url": "/final-year-frontend/index.html"
+  },
+  {
+    "revision": "d6f9ae74cf785c3bf829",
+    "url": "/final-year-frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5e9ee25087910e457e87",
+    "url": "/final-year-frontend/static/css/main.03d440f1.chunk.css"
+  },
+  {
+    "revision": "d6f9ae74cf785c3bf829",
+    "url": "/final-year-frontend/static/js/2.7f71288e.chunk.js"
+  },
+  {
+    "revision": "242993bfc9f71c9c866d8da8b903d7f7",
+    "url": "/final-year-frontend/static/js/2.7f71288e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9ee25087910e457e87",
+    "url": "/final-year-frontend/static/js/main.c33c4b41.chunk.js"
+  },
+  {
+    "revision": "cab4b3b1a8c0ddd0fb09",
+    "url": "/final-year-frontend/static/js/runtime-main.73319852.js"
+  },
+  {
+    "revision": "8fe6337535aeaace37a79eb3eb6252a4",
+    "url": "/final-year-frontend/static/media/caret.8fe63375.png"
+  },
+  {
+    "revision": "5627f66c2b60e3dba1ca5ec66fa72908",
+    "url": "/final-year-frontend/static/media/portalfront.5627f66c.jpg"
+  },
+  {
+    "revision": "2c319e5908de27813512e64b3b180493",
+    "url": "/final-year-frontend/static/media/stage1.2c319e59.png"
+  },
+  {
+    "revision": "0e2387b81678a6d2958820c32852f591",
+    "url": "/final-year-frontend/static/media/stage3.0e2387b8.png"
+  },
+  {
+    "revision": "d1826015d458a030a28f932535bc384d",
+    "url": "/final-year-frontend/static/media/stage4.d1826015.png"
+  },
+  {
+    "revision": "900a2ad7efdfdea67ae513006f7140c0",
+    "url": "/final-year-frontend/static/media/welcomeImg.900a2ad7.jpg"
+  }
+]);
